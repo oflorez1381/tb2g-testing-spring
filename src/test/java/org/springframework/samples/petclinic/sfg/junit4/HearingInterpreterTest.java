eb28b1687651aa8853976332c632296dd5cb45aa
oflorez <v-oflorez@expedia.com>
2020-05-07 20:22:36 -0400

    13.7 Junit 5

--- src/test/java/org/springframework/samples/petclinic/sfg/junit4/HearingInterpreterTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit4/HearingInterpreterTest.java
@@ -1,24 +1,27 @@
-package org.springframework.samples.petclinic.sfg;
+package org.springframework.samples.petclinic.sfg.junit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.sfg.BaseConfig;
+import org.springframework.samples.petclinic.sfg.HearingInterpreter;
+import org.springframework.samples.petclinic.sfg.LaurelConfig;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = { BaseConfig.class, YannyConfig.class })
-public class HearingInterpreterYannyTest {
+@ContextConfiguration(classes = { BaseConfig.class, LaurelConfig.class })
+public class HearingInterpreterTest {
 
     @Autowired
     HearingInterpreter hearingInterpreter;
 
     @Test
-    public void whatIheard() {
+    public void whatIHeard(){
         String word = hearingInterpreter.whatIheard();
-
-        assertEquals("Yanny", word);
+        assertEquals("Laurel", word);
     }
+
 }
