a10927e06f3b3c9d5b464319f68e5770e1af5792
oflorez <v-oflorez@expedia.com>
2020-05-07 22:59:01 -0400

    13.12 Setting Active Profiles for Tests

--- src/test/java/org/springframework/samples/petclinic/sfg/junit4/HearingInterpreterLaurelTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit4/HearingInterpreterLaurelTest.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.sfg.BaseConfig;
 import org.springframework.samples.petclinic.sfg.HearingInterpreter;
 import org.springframework.samples.petclinic.sfg.LaurelConfig;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
+@ActiveProfiles("base-test")
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = { BaseConfig.class, LaurelConfig.class })
 public class HearingInterpreterLaurelTest {
