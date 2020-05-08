980ac8e9d4448f15b834ce8cbf0e005c739e5b6b
oflorez <v-oflorez@expedia.com>
2020-05-07 20:35:16 -0400

    13.9 Junit 5 Yanny Interpreter

--- src/test/java/org/springframework/samples/petclinic/sfg/junit4/HearingInterpreterLaurelTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit4/HearingInterpreterLaurelTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = { BaseConfig.class, LaurelConfig.class })
-public class HearingInterpreterTest {
+public class HearingInterpreterLaurelTest {
 
     @Autowired
     HearingInterpreter hearingInterpreter;
